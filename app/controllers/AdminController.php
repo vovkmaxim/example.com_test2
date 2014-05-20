479757ecaef38950b2ed10572440861c4c0f3103
php <php@php-System-Product-Name>
2014-05-20 14:46:21 +0300

    create constructor for work service islogged

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,6 +2,15 @@
 
 class AdminController extends BaseController {
 
-	
+	public function __construct() {
+            try {
+                App::make('user_provaider')->islogged();
+            } catch (\Exception $e) {
+                $news = array(
+                    "error" => $e->getMessage()
+                );
+                return View::make('getErrorApi', $news);
+            }
+        }
 
 }
