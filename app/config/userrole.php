5c0b2dad600fe9fb464ecbeb109ac6c89b084160
php <php@php-System-Product-Name>
2014-05-21 18:06:52 +0300

    add user role name

---
+++ app/config/userrole.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+
+	'admin' => array(
+                        'home','login','all-news','one-news','change-news','create-news','search-news','tag-search-news',
+                        'registration','logout','delete-news',
+                    ),
+	'manager' => array(
+                        'home','login','all-news','one-news','change-news','create-news','search-news','tag-search-news',
+                        'registration','logout'
+                    ),
+
+	'editor' => array(
+                        'home','login','all-news','one-news','search-news','tag-search-news',
+                        'registration','logout'
+                    ),
+    );
