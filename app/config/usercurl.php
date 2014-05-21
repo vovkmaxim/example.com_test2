4092e31e1cb76dc2524806f7f418b2373285a11c
php <php@php-System-Product-Name>
2014-05-21 14:10:11 +0300

    add user role api link

--- app/config/usercurl.php
+++ app/config/usercurl.php
@@ -9,5 +9,7 @@ return array(
 	'islogged' => 'http://test1.com/api.test1.api-islogged',
     
 	'logout' => 'http://test1.com/api.test1.api-logout',
+    
+	'role' => 'http://test1.com/api.test1.api-role',
 
 );
