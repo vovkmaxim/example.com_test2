c684f28f00299e896186fa1678597e9349a8e6aa
php <php@php-System-Product-Name>
2014-05-22 13:31:00 +0300

    add 2 role aditor1 and editor2

--- app/config/userrole.php
+++ app/config/userrole.php
@@ -3,7 +3,7 @@
 return array(
 
 	'admin' => array(
-                        'home','login','all-news','one-news','change-news','create-news','search-news','tag-search-news',
+                        'home','login','all-news','search-news','tag-search-news',
                         'registration','logout','delete-news',
                     ),
 	'manager' => array(
@@ -14,5 +14,13 @@ return array(
 	'editor' => array(
                         'home','login','all-news','one-news','search-news','tag-search-news',
                         'registration','logout'
+                    ),
+	'editor1' => array(
+                        'home','login','all-news','one-news','tag-search-news',
+                        'registration','logout',
+                    ),
+	'editor2' => array(
+                        'home','login','all-news','search-news','tag-search-news',
+                        'registration','logout',
                     ),
     );
