711c9e46e7aae95950f4cb16bdf4bcf29455d869
php <php@php-System-Product-Name>
2014-05-26 19:11:56 +0300

    upload image form

--- app/views/uploadImageForm.blade.php
+++ app/views/uploadImageForm.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
     @section('header')
-    	<title>Registration</title>
+    	<title>Image upload</title>
     @endsection
     
     @section('content')
@@ -10,7 +10,12 @@
     <?php
     $str = "";
     foreach ($images as $imag){
-        $str .= '<img alt="Laravel" src="http://example.com/uploads/images/original/'.$imag.'" width="100" height="111"><br><br>';
+        $str .= Form::open(array('url' => 'delete-image', 'files' => true)).
+                '<img alt="Laravel" src="http://example.com/uploads/images/original/'
+                .$imag.'" width="100" height="111">  '
+                .Form::hidden('image-name',$imag)
+                .Form::submit('delete',array('class' => 'btn  '))
+                .Form::close();
     }
     print_r("<pre>");
     echo $str;
@@ -18,14 +23,14 @@
     
     ?>
     
-            @if (!$errors->isEmpty())
+        @if (!$errors->isEmpty())
         <div class="alert alert-danger">
             @foreach ($errors->all() as $error)
             <p>{{ $error }}</p>
             @endforeach
         </div>
         @endif
-    
+        @if($imageCount)
         {{ Form::open(array('url' => 'upload-image', 'files' => true)) }}
         
             {{ Form::text('title') }}
@@ -35,5 +40,6 @@
             {{ Form::submit('Upload',array('class' => 'btn btn-lg btn-primary ')) }}
             
         {{ Form::close() }}
+        @endif
     @endsection
 </div>
