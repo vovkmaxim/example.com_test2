5d9d3b0e3b56ed19ade8b561982f50a0f5545844
php <php@php-System-Product-Name>
2014-05-26 19:10:28 +0300

    send emails form

---
+++ app/views/emails/emails.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h2>TEST MESSAGE!!!</h2>
+ 
+    
+    <div>Subject: {{ $subject }}</div>
+    <div>From: {{ $email }}</div>
+    <div>Message: {{ $text }} </div>
+  </body>
+</html>
